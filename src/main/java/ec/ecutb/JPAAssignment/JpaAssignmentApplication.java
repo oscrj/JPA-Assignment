b286f1ea5a402a4b505109511e24691f172ee879
oscrj <oscar.johanneson@gmail.com>
2020-02-28 10:23:05 +0100

    First Commit.

---
+++ src/main/java/ec/ecutb/JPAAssignment/JpaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package ec.ecutb.JPAAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/ecutb/JPAAssignment/JpaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.ecutb.JPAAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
