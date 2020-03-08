6fa8c52dd8b3e9ec07f702427860fc9ef38de251
oscrj <oscar.johanneson@gmail.com>
2020-03-08 20:31:33 +0100

    Service layer.

--- src/main/java/ec/ecutb/JPAAssignment/CommandLine.java
+++ src/main/java/ec/ecutb/JPAAssignment/CommandLine.java
@@ -2,13 +2,11 @@ package ec.ecutb.JPAAssignment;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class CommandLine implements CommandLineRunner {
 
     @Override
-    @Transactional
     public void run(String... args) throws Exception {
 
     }
